fc83ee46427c401a2e260bc958f7e04b9292b5df
juhatae <juhatae@localhost>
2023-05-24 15:41:25 +0900

    저

--- vcsTest/src/vcsTest/Hello.java
+++ vcsTest/src/vcsTest/Hello.java
@@ -1,7 +1,7 @@
 package vcsTest;
 
 public class Hello {
- public static void main(String[] args) {
-	System.out.println("Hello world!");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
 }
