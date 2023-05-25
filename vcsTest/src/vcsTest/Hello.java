1611a844f37083fe3461151414d663aa5e1a54d0
juhatae <juhatae@192.168.50.24>
2023-05-25 10:08:54 +0900

    첫번째 문장 작

--- vcsTest/src/vcsTest/Hello.java
+++ vcsTest/src/vcsTest/Hello.java
@@ -1,14 +1,13 @@
 package vcsTest;
-
 public class Hello {
 	public static void main(String[] args) {
+		// 로컬에서 작업했습니다.
 		System.out.println("Hello world!");
 		
 		System.out.println("변경");
 		
-		System.out.println("빵상");
-		
 		System.out.println("나는다섯번째인데");
+		
 		//주석
 	}
 }
